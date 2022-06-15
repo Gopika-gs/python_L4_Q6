d87dd625e40e32e6a112c5978726b4141c7e7263
Gopika-gs <gsgopika00@gmail.com>
2022-06-15 11:57:31 +0530

    corrected errors

--- L4_Q6.py
+++ L4_Q6.py
@@ -1,2 +1,9 @@
-list = [1,2,3,4,5]
-print(sum(list))
+def sum_of_list(my_list):
+    if not my_list:
+        return 0
+    elif len(my_list) == 1:
+        return my_list[0]
+    else:
+        return my_list[0] + sum_of_list(my_list[1:])
+
+print(sum_of_list([1,2,3,4,5,6,7]))
